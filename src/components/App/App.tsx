6d3470242fdedcc2e9c3bb218cf2799e6a06ed5b
irinasvetlichnaya <irinasvetlichnaya16@gmail.com>
2025-06-02 00:54:54 +0300

    added project structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import css from './App.module.css'
+
+export default function App() {
+ 
+
+  return (
+    <div className={css.app} >
+    </div>
+  )     
+}
+
+
